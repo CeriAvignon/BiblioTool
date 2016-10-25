030d59f572fb876ea3beb3b59418cd24641ca61d
NicolasDuret <nico.duret@orange.fr>
2016-10-25 18:05:06 +0200

    test liaison entre package

--- src/Test3.java
+++ src/Test3.java
@@ -1,5 +1,8 @@
+import TestFolder.ClassFolder;
 public class Test3 {
 	public static void main(String[] args) {
+		ClassFolder cf = new ClassFolder(3);
+		System.out.println(cf.getX());
 		System.out.println("Hello world");
 	}
 }
--- src/TestFolder/ClassFolder.java
+++ src/TestFolder/ClassFolder.java
@@ -1,5 +1,13 @@
 package TestFolder;
 
 public class ClassFolder {
-
+	private int x;
+	
+	public ClassFolder(int x){
+		this.x = x;
+	}
+	
+	public int getX(){
+		return x;
+	}
 }
