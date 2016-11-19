68684d94e01d9ce886ca6e867af6d7e3ba2bdca0
NicolasDuret <nico.duret@orange.fr>
2016-11-19 16:26:43 +0100

    Supression des fichiers inutiles de test

--- src/Test3.java
+++
@@ -1,8 +0,0 @@
-import TestFolder.ClassFolder;
-public class Test3 {
-	public static void main(String[] args) {
-		ClassFolder cf = new ClassFolder(3);
-		System.out.println(cf.getX());
-		System.out.println("Hello world");
-	}
-}
