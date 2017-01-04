8fafd4ea505325c4390fa3638566ad7310d84e2c
fatimaze <fatima.zemmahi19@gmail.com>
2017-01-04 20:21:50 +0100

    Update WorldCat.java

--- src/fatima/WorldCat.java
+++ src/fatima/WorldCat.java
@@ -1,13 +1,13 @@
-package searchingmodule.libraries.worldCat;
+package fatima;
 
 public class WorldCat extends Library implements Runnable{
-    private String author;
+    private String auteur;
     private String article;
     private String uri;
     
     public WorldCat(String author, String article, String uri) {
-        System.out.println("WorldCat Search");
-        this.author = author;
+        System.out.println("recherch worldcat");
+        this.auteur = author;
         this.article = article;
         this.uri = uri;
     }
@@ -15,7 +15,7 @@ public class WorldCat extends Library implements Runnable{
     @Override
     public void run() {
         System.out.println("WorldCat Implementation");
-        WorldCatSearch WC = new WorldCatSearch(author, article, uri);
+        WorldCatSearch WC = new WorldCatSearch(auteur, article, uri);
     }
     
 }
