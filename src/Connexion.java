65d4105c01f9d1466ab866dec5287ae37782acea
gous2016 <tgoussoumalet@yahoo.fr>
2017-01-10 14:17:28 +0100

    Add files via upload

---
+++ src/Connexion.java
@@ -0,0 +1,41 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connexion;
+import java.sql.*;
+/**
+ *
+ * @author Tanguy
+ */
+public class Connexion {
+  
+   
+  public static void main(String[] args) {
+      Connection cnx= ConnecterDB();
+      
+    }
+   public static Connection ConnecterDB(){
+         String url="jdbc:mysql://localhost:3306/bd-biblio";
+         String user="root";
+         String password="";  
+         
+         
+        try{
+            //chargement du driver
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver ok");
+            //recuperation de la connexion
+            Connection cnx= DriverManager.getConnection(url,user,password);
+            System.out.println("connexion bien établie");
+            
+            return cnx;
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            return null;
+             }
+   }
+}
+ 
