93d4557b630da29e83801142404dcb0f46392982
NicolasDuret <nico.duret@orange.fr>
2017-02-28 21:58:47 +0100

    rajout que lorsque la souris passe par dessus un domaine cela ouvre une petite fenetre qui affiche l'intérieur du domaine.

--- src/graphical_interface/Player.java
+++ src/graphical_interface/Player.java
@@ -9,8 +9,12 @@ package graphical_interface;
  *
  */
 
+import java.awt.Dimension;
 import java.awt.event.*;
-//import javax.swing.SwingUtilities; //peut servir si j'ai besoin d'un check de click
+
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+//import javax.swing.JOptionPane;
 
 public class Player implements MouseMotionListener
 {
@@ -23,20 +27,24 @@ public class Player implements MouseMotionListener
 	@Override
 	public void mouseDragged(MouseEvent e) {
 	}
-
+	
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		if(pan.getOval().contains(e.getX(), e.getY()))
+		if(pan.getOval().contains(e.getX(), e.getY())) {
+			//JOptionPane.showMessageDialog(null, "My Goodness, this is so concise");
+			/*final JDesktopPane desktop = new JDesktopPane();
+			
+			final JInternalFrame newwindow = new JInternalFrame("internal window", true, true, true, true);
+			newwindow.dispose();
+			newwindow.setVisible(true);
+			newwindow.setSize(300,240);
+			newwindow.setMinimumSize(new Dimension(300,240));
+			desktop.add(newwindow);
+			newwindow.moveToFront();*/
+			
 			System.out.println("oui");
-		else
-			System.out.println("non");
-		
+		}
 	}
-	
-	public void setCenter() {	//not implemented now
-		pan.setLocation(0,0);
-	}
-
 }
 
 
