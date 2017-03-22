caa159346292dd945a653b120681411c449bba7b
Benhachani Mohamed <mohamed.benhachani@alumni.univ-avignon.fr>
2017-03-22 18:13:18 +0100

    Add files via upload

--- src/textmining/Bibtex.java
+++ src/textmining/Bibtex.java
@@ -1,4 +1,5 @@
 package textmining;
+
 import java.io.BufferedReader;
 import java.io.FileReader ;
 import java.io.FileNotFoundException;
@@ -17,6 +18,7 @@ public class Bibtex {
 	private String type_de_doc;
 	private String ref;
 	private Map<String, String> donne;
+	private Map<String, String> String_var;
 	private int i;
 	public  Bibtex() { 
 
@@ -24,11 +26,26 @@ public class Bibtex {
 		type_de_doc="";
 		ref="";
 		donne = new HashMap<>();
+		String_var=new HashMap<>();
+		i=0;
 
 	}
- 	
- 	public void affiche(){
- 		Set<Entry<String, String>> setHm = donne.entrySet();
+
+ 	 public void affiche(){
+ 	 System.out.println(type_de_doc+"  \n\nDONNE : ");
+ 	 affiche_objet(donne);
+ 	  System.out.println("  \n\nVARIABLE : ");
+ 	 affiche_objet(String_var);
+ 	 
+ 	  	  System.out.println("  \n\nTEST : ");
+ 	 
+ 	 if(String_var.get("foo") == null )
+		 	System.out.println("VARIABLE NULL'foo'");
+		 else 
+		 	System.out.println(String_var.get("foo"));//geter si NULL // int
+ 	 }
+ 	private void affiche_objet(Map<String, String> objet){
+ 		Set<Entry<String, String>> setHm = objet.entrySet();
 
 	      Iterator<Entry<String, String>> it = setHm.iterator();
 
@@ -36,7 +53,7 @@ public class Bibtex {
 
 		 Entry<String, String> e = it.next();
 
-		 System.out.println(e.getKey() + " : " + e.getValue());
+		 System.out.println(e.getKey() + " : " + e.getValue()+"\n");
 
 	      }
  	
@@ -64,7 +81,6 @@ public class Bibtex {
 		
   	//	System.out.println(str);
   		
-  		 Map<String, String> String_var= new HashMap<>();;
   		String tampon="";
   		int mode=-1;
   		
@@ -77,9 +93,10 @@ public class Bibtex {
 		 	if(str.charAt(i) == '@'){
 
 		 		while(++i<str.length() && str.charAt(i)!= '{' && str.charAt(i)!= '(')
-		 			tampon+=str.charAt(i);	
+		 			if(str.charAt(i)!= ' ')
+		 				tampon+=str.charAt(i);	
 		 			
-		 			
+		 		System.out.println("1T !: "+tampon);
 		 		if(i < str.length()){
 		 		
 		 			if(tampon.equalsIgnoreCase("Preamble") || tampon.equalsIgnoreCase("Comment") ){//commentaire ou preambule #onsenfou
@@ -89,21 +106,24 @@ public class Bibtex {
 		 			else{
 		 				if(tampon.equalsIgnoreCase("String")){//C'est une varaible de type string
 		 					 Extraire(str,str.charAt(i),String_var);
-		 					
+		 					System.out.println("2T !: "+tampon);
 		 				}
 		 				else{
+		 					System.out.println("3T !: "+tampon);
 		 					type_de_doc=tampon;
 		 					
 					 		//System.out.println(str.charAt(i));
 					 		
 					 		char init =str.charAt(i);
-					 		
+					 		int in = i+1;
 					 		while(++i<str.length() && str.charAt(i)!= ',' && str.charAt(i)!= '=')
 					 			ref+=str.charAt(i);
 					 		
-					 		if(str.charAt(i) != ',')
+					 		if(str.charAt(i) != ','){
 					 			ref="";
-		 					 Extraire(str,init,String_var);
+					 			i=in;
+					 		}
+		 					 Extraire(str,init,donne);
 					 	}
 			 		}
 			 	}
@@ -111,7 +131,7 @@ public class Bibtex {
 		 	
 		 		
 		 			
-		 	
+		 	tampon="";
 		 	
 		 	
 		 }
@@ -176,20 +196,23 @@ public class Bibtex {
  		}
  		
 		while(++i<str.length()){
+		
 			//System.out.println(str.charAt(i));
 			if(str.charAt(i) == '='){
 			
-				while(++i<str.length() && str.charAt(i)!= '{' && str.charAt(i)!= '"');	//int sans rien "" ou {}
+				//while(++i<str.length() && str.charAt(i)== ' ' && str.charAt(i)!= '"');	//int sans rien "" ou {}				
+				i++;
 
-				if(i < str.length())
-					Valeur( str,str.charAt(i),objet,cle);
+				if(i < str.length())		//Si c'est le fichier est pas finie
+					Valeur( str,fin,objet,cle);
 				//else false
 				
-				System.out.println(cle);
+				//System.out.println(cle);
 				cle="";
 			}
 			else
-				cle+=str.charAt(i);
+				if(str.charAt(i) != ' ')
+					cle+=str.charAt(i);
 			
 			if(str.charAt(i) == ',')
 				cle="";
@@ -197,9 +220,10 @@ public class Bibtex {
 
 			if(str.charAt(i) == fin && str.charAt(i-1) != '\\'){ //nombre paire de \ pour anuler
  				nb_init--;
- 				
+
  				if(nb_init == 0){
  					i++;
+ 					
  					return true;
  					
  				}
@@ -214,48 +238,140 @@ public class Bibtex {
  	
  	
  	}
- 	public boolean Valeur(String str,char init,Map<String, String> objet,String cle){
+ 	public boolean Valeur(String str,char FIN_final,Map<String, String> objet,String cle){
  	
- 		//System.out.println("ok "+init);
- 	
- 		int nb_init=1;
+ 		for(;i<str.length() && str.charAt(i) ==' ';i++);
+
+		System.out.println(str.charAt(i) +"<--");
+		
+		int nb_init=1;
  		char fin;
+ 		char init=str.charAt(i);
  		
- 		if(init == '"')
+ 		 String tmp="";
+ 		String Tampon="";
+ 		
+ 		if( init == '"')
  			fin='"';
  		else{
 	 		
 	 		if(init == '{')
 	 			fin='}';
-	 		else 
-	 			return false;
+	 		else {
+				fin='V';
+				i--;
+			}
  		
  		}
  		
 		while(++i<str.length()){
 
 
-			if(str.charAt(i) == fin && str.charAt(i-1) != '\\'){ //nombre paire de \ pour anuler
+			if(fin != 'V' && str.charAt(i) == fin && str.charAt(i-1) != '\\'){ //nombre paire de \ pour anuler
 			
  				nb_init--;
  							
  				if(nb_init == 0){
+ 				
+ 					Tampon += tmp;
+ 					//System.out.println(cle +","+Tampon);
+ 					System.out.println("!! "+nb_init+" :: "+FIN_final);
+ 					
  					i++;
- 					return true;
+
  					
  				}
  				
  			}
- 			if(str.charAt(i) == init && str.charAt(i-1) != '\\')//pour l'instant sa suffit
- 				nb_init++;
+ 			if(fin == 'V'){
  			
- 			System.out.print(str.charAt(i));
+ 				if((str.charAt(i) == ' ' || str.charAt(i) == '"' || str.charAt(i) == '{' ) && str.charAt(i-1) != '\\'){//beh NON fin # , FIN_final
+ 					
+ 					if(String_var.get(tmp) == null ){
+ 					
+					 	System.out.println("VARIABLE NULL'"+tmp+"'");
+					 	Tampon +=Integer.parseInt(tmp);
+					 }
+					 else 
+					 	 Tampon +=String_var.get(tmp);//geter si NULL // int
+					 	
+ 					
+ 					
+ 					//System.out.println(cle,Tampon);
+					 nb_init=0;
+ 					 i++;
+ 				
+ 				}
+ 				
+ 			} 
+ 			if(nb_init !=0 && init == '{' && str.charAt(i) == init && str.charAt(i-1) != '\\')//pour l'instant sa suffit
+ 				nb_init++;
+ 				
+ 			if((str.charAt(i) == ',' || str.charAt(i) == FIN_final) && (fin =='V' || nb_init==0)){
+ 			
+ 				if(str.charAt(i) == FIN_final && nb_init==0)
+					i--;
+				
+				 System.out.println(cle+" : "+tmp+":" +str.charAt(i)+" : "+nb_init);
+				 
+				if(nb_init > 0){
+					if(fin == 'V'){
+						 if(String_var.get(tmp) == null ){
+						 	System.out.println("VARIABLE NULL'"+tmp+"'");
+						 	Tampon +=Integer.parseInt(tmp);
+						 }
+						 else 
+						 	 Tampon +=String_var.get(tmp);//geter si NULL // int
+						 	
+						 System.out.println("VARIABLE");
+						 
+					}
+					else
+						 Tampon += tmp; 
+						 
+				}	 
+					 
+				objet.put(cle,Tampon);
+
+				return true;
+ 				
+ 			}
+ 			if(nb_init == 0 && str.charAt(i) == '#' ){
+ 			
+ 				System.out.println("IL Faut concatener");
+ 			
+ 			}	
+ 			tmp += str.charAt(i);
+ 			//System.out.println(tmp);
 
 		}
- 	 	
  		return false;//terminer sans fin
  	
  	}
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
 }
-
 
