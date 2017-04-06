59bc9cb8d7a1d6da396220d1d96bb9d540b470ef
SaoussanTRIGUI <trigui.yasan@gmail.com>
2017-04-06 10:46:30 +0200

    Extraire le graph par cocitation (co_cité)
    1)ArrayList<ArrayList<Node>> ReturnNodesCocite(){}
    2)DirectedGraph createUndirectedGraph(ArrayList<ArrayList<Node>>
    listeNodeCoCite){}

--- src/graphe/MyGraph.java
+++ src/graphe/MyGraph.java
@@ -1,21 +1,27 @@
 package graphe;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.gephi.graph.api.Column;
 import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.UndirectedGraph;
+
 import org.gephi.graph.api.Edge;
+import org.gephi.graph.api.Graph;
 import org.gephi.graph.api.GraphController;
 import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.Node;
+import org.gephi.graph.api.EdgeIterable;
+
 import org.gephi.project.api.ProjectController;
 import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
-
+import org.gephi.datalab.api.AttributeColumnsController;
+import org.gephi.graph.*;
 public final class MyGraph {
 	
+	private static final String NULL = null;
 	public static List<Article> articles;
 	public static List<Reference> references;
 	public static GraphModel graphModel;
@@ -32,8 +38,8 @@ public final class MyGraph {
 		DirectedGraph directedGraph = graphModel.getDirectedGraph();
 		createColumns();
 		
-		articles = listeArticles();
-		references = listeReference();
+		articles = retournerListeArticles();
+		references = ListeReference();
 		
 		for (Article article : articles) {
 			Node n0 = graphModel.factory().newNode(String.valueOf(article.getIdArt()));
@@ -51,12 +57,13 @@ public final class MyGraph {
 			n0.setAttribute(urlArt, article.getUrlArt());
 			//n0.setAttribute(ref, article.getReferences());
 			n0.setAttribute(status, article.getStatus());
-			
 			directedGraph.addNode(n0);
 		}
 		
 		System.out.println("les attributs du node sont :");
 			for (Column col : graphModel.getNodeTable()) {
+				
+
 			System.out.println(col);
 		}
 			
@@ -67,17 +74,6 @@ public final class MyGraph {
 		}
 		
 		return directedGraph;
-	}
-	
-	public static void createAuthorsGraph(){
-		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
-		pc.newProject();
-		Workspace workspace = pc.getCurrentWorkspace();
-		
-		graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
-		DirectedGraph authorGraph = graphModel.getDirectedGraph();
-		
-		
 	}
 	
 	public static void createColumns(){
@@ -97,7 +93,6 @@ public final class MyGraph {
 		status = graphModel.getNodeTable().addColumn("status", Boolean.class);
 	}
 	
-	
 	public static List<Article> getArticles() {
 		return articles;
 	}
@@ -115,29 +110,23 @@ public final class MyGraph {
 	}
 
 	// methode implementee par le groupe web-mining
-	public static List<Article> listeArticles() {
+	public static List<Article> retournerListeArticles() {
 		List<Article> articles = new ArrayList<Article>();
 		for (int i = 0; i < 6; i++) {
 			Article art = new Article();
 			art.setIdArt(i + 1);
 			art.setTitleArt("article" + (i + 1));
-			art.setAuthor(testListAuthor());
+			art.setNbPage(50+i);
+			art.setNumVol(700);
+			art.setDoi("AIIII001");
+			art.setStatus(true);
 			articles.add(art);
 		}
 		return articles;
 	}
 
-	public static List<Author> testListAuthor(){
-		List<Author> test = new ArrayList<Author>();
-		for (int i = 0; i < 5; i++) {
-			Author a = new Author(i, "nom_author"+i+1, "prenom_author"+i+1, "affiliation"+i+1);
-			test.add(a);
-		}
-		return test;
-	}
-	
 	// methode implementee par le groupe web-mining
-	public static List<Reference> listeReference() {
+	public static List<Reference> ListeReference() {
 		List<Reference> references = new ArrayList<Reference>();
 		for (int i = 1; i <= 5; i++) {
 			Reference ref = new Reference();
@@ -145,31 +134,162 @@ public final class MyGraph {
 			ref.setTarget(i + 1);
 			references.add(ref);
 		}
+		Reference ref1 = new Reference();
+		ref1.setSource(5);
+		ref1.setTarget(3);
+		references.add(ref1);
+		Reference ref2 = new Reference();
+		ref2.setSource(5);
+		ref2.setTarget(2);
+		references.add(ref2);
+		
+		Reference ref3 = new Reference();
+		ref3.setSource(3);
+		ref3.setTarget(1);
+		references.add(ref3);
+		
 		return references;
 	}
+
 	
-	public static List<Author> listAllAuthors(){
-		List<Author> authors = new ArrayList<Author>();
-		List<Article> articles = listeArticles();
-		Iterator<Author> iterate_auth;
-		for (Article article : articles) {
-			iterate_auth = article.getAuthor().iterator();
-			while(iterate_auth.hasNext()){
-				Author auth = iterate_auth.next();
-				authors.add(auth);
-			}
+	
+	public static List<String> nodeInfo(List<Node> selectedNodes)
+	{
+		int nbColumn=0;
+		List<String> ListInfoSelectedNodes=new ArrayList<String>();
+		
+		for (Column col : graphModel.getNodeTable())
+		{
+			nbColumn++;
 		}
-		return authors;
+
+		
+		for (int i=0; i <selectedNodes.size(); i++) //parcourir la liste selectedNodes envoyée par le groupe IG
+		{	
+			String stringInfoNode="";
+			for (int j=0;j<nbColumn;j++)//parcourir les informations du noeud
+			{
+				Column col=graphModel.getNodeTable().getColumn(j);
+				
+				if(((selectedNodes.get(i)).getAttribute(col)!=NULL)&&(col.isIndexed()==true))
+				{
+					String NodeAttribute=(selectedNodes.get(i)).getAttribute(col).toString();
+					stringInfoNode=""+stringInfoNode+" "+col.getTitle()+": "+NodeAttribute+", ";
+				}
+			}
+			ListInfoSelectedNodes.add(i,stringInfoNode);
+		}
+		return (ListInfoSelectedNodes);
 	}
 	
-	public static List<Author> listAuthorsByArticle(Article article){
-		List<Author> authors = new ArrayList<Author>();
-		Iterator<Author> iterate_auth = article.getAuthor().iterator();
-		while(iterate_auth.hasNext()){
-			Author auth = iterate_auth.next();
-			authors.add(auth);
+	
+	
+	
+	
+	
+	
+	
+	/*************CoCité******************/
+	public static ArrayList<ArrayList<Node>> returnNodesCocite()
+	{
+		int nbLigne=0;
+		DirectedGraph directedGraph1 = createDirectedGraph();
+		nbLigne=directedGraph1.getEdgeCount();
+		System.out.println("numberRow: " + nbLigne);
+		EdgeIterable eI=directedGraph1.getEdges();//iterateur sur le tableau des arcs
+		Edge[] tabEdges=eI.toArray();//tableau des edges
+
+		ArrayList<ArrayList<Node>> listeNodeCoCite = new ArrayList<ArrayList<Node>>();
+		
+		for (int i=0; i <tabEdges.length; i++) 
+		{
+			System.out.println("Les information du noeud "+tabEdges[i].getSource().getLabel()+"->"+tabEdges[i].getTarget().getLabel());
+		}
+
+		
+		for(int i=0;i<nbLigne-1;i++)
+		{
+
+			ArrayList<Node> listeOfNodesCiteeParNodes= new ArrayList<Node>();
+			listeOfNodesCiteeParNodes.add(tabEdges[i].getTarget());
+
+			for(int j=i+1;j<nbLigne;j++)
+			{
+
+				if(tabEdges[i].getSource().getLabel()==tabEdges[j].getSource().getLabel())
+				{
+
+					listeOfNodesCiteeParNodes.add(tabEdges[j].getTarget());
+					
+				}
+			}
+			listeNodeCoCite.add(listeOfNodesCiteeParNodes);
+					
 		}
 		
-		return authors;
+		return listeNodeCoCite;
+
 	}
+	
+	
+	/*Le resultat doit être undirected graph, mais pour le moment j'ai fait directed graph*/
+	public static DirectedGraph createUndirectedGraph(ArrayList<ArrayList<Node>> listeNodeCoCite){
+		DirectedGraph directedGraph1 = createDirectedGraph();
+		EdgeIterable eI=directedGraph1.getEdges();//iterateur sur le tableau des arcs
+		Edge[] tabEdges=eI.toArray();//tableau des edges
+		for (int i=0; i <tabEdges.length; i++) 
+		{
+			directedGraph1.removeEdge(tabEdges[i]);//suppression de tous les anciens arcs entre les sommets
+        }
+		int nbLigne=directedGraph1.getEdgeCount();
+        System.out.println("nbEdges:"+nbLigne);
+		 ArrayList<Node> n=new ArrayList<Node> ();
+
+        for(int h =0;h<listeNodeCoCite.size();h++)
+		{
+			n = listeNodeCoCite.get(h);
+			if(n.size()>1)
+			{
+				for(int j=0;j<n.size()-1;j++)
+	
+				{
+					for(int k=j+1;k<n.size();k++)
+						
+					{
+					
+						Edge e1 = graphModel.factory().newEdge(n.get(j),n.get(k)); //creation de nouveaux arcs basés sur la relation de co_cité
+						directedGraph1.addEdge(e1);
+					}
+				}
+		
+			}
+		}
+	
+	
+        
+     /*Creation de undirected graph n'est pas encore terminée*/   
+	/*	ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+		pc.newProject();
+		Workspace workspace = pc.getCurrentWorkspace();
+		graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
+		UndirectedGraph unDirectedGraph = graphModel.getUndirectedGraph();
+		createColumns();
+	*/
+        
+		
+		return directedGraph1;
+	}
+	
+	/*******/
+	
+	
+	
+	/******/
+	
+	
+	
+	
+	
+	
 }
+
