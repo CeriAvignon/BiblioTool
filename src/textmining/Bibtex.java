05ee50fbdc199b297a69fcebcacf5dc4b36bf986
MOHAMED <MOHAMED@MOHAMED-PC>
2017-04-26 05:30:45 +0200

    last version

--- src/textmining/Bibtex.java
+++ src/textmining/Bibtex.java
@@ -1,4 +1,22 @@
+
+ /*     Copyright (C) 2016 Benhachani Mohamed
+*
+ *   This program is free software; you can redistribute it and/or
+  *  modify it under the terms of the GNU Lesser General Public
+   * License as published by the Free Software Foundation; either
+    *version 2.1 of the License, or (at your option) any later version.
+*
+ *   This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    *Lesser General Public License for more details.
+*
+ *   You should have received a copy of the GNU Lesser General Public
+  *  License along with this program; if not, write to the Free Software
+   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 package textmining;
+
 
 import java.io.BufferedReader;
 import java.io.FileReader ;
@@ -13,6 +31,13 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.Map.Entry;
 
+ /**
+ * Bibtex est une classe qui peut lire des fichier bibtex
+
+ * 
+ * @author Benhachani Mohamed
+ * @version 1.0
+ */
 public class Bibtex {
 
 	private String type_de_doc;
@@ -20,6 +45,15 @@ public class Bibtex {
 	private Map<String, String> donne;
 	private Map<String, String> String_var;
 	private int i;
+	
+	
+	
+	/**
+         * Constructeur de Bibtex. 
+	 *
+         * A la construction d'un objet Bibtex 
+
+         */
 	public  Bibtex() { 
 
 
@@ -30,19 +64,35 @@ public class Bibtex {
 		i=0;
 
 	}
+	/*
+	public static void main(String[] args) {
 	
+		//Exemple avec un fichier
+			/*Bibtex B = new Bibtex();
+			B.ReadFromFile("../science.bib");
+			B.affiche();
+		
+			B.ArticleFromBibtex().ViewArticle();
+			
+		
+		//Exemple avec un string 
+		
+		
+		
+
+ 	}*/
+ 	/**
+         * Affiche les donnee de l'objet Bibtex. 
+	 *
+
+         */
  	 public void affiche(){
- 	 System.out.println(type_de_doc+"  \n\nDONNE : ");
- 	 affiche_objet(donne);
- 	  System.out.println("  \n\nVARIABLE : ");
- 	 affiche_objet(String_var);
- 	 /*
- 	  	  System.out.println("  \n\nTEST : ");
- 	 
- 	 if(String_var.get("foo") == null )
-		 	System.out.println("VARIABLE NULL'foo'");
-		 else 
-		 	System.out.println(String_var.get("foo"));//geter si NULL // int*/
+	 	 System.out.println(type_de_doc+"  \n\nDONNE : ");
+	 	 affiche_objet(donne);
+	 	  System.out.println("  \n\nVARIABLE : ");
+	 	 affiche_objet(String_var);
+	 	 System.out.println("  \n\n\n");
+	 
  	 }
  	private void affiche_objet(Map<String, String> objet){
  		Set<Entry<String, String>> setHm = objet.entrySet();
@@ -58,6 +108,52 @@ public class Bibtex {
 	      }
  	
  	}
+ 	/**
+         * Affiche les donnee de l'objet Bibtex. 
+         */
+ 	public Article ArticleFromBibtex(){
+ 	
+ 		Article A = new Article();
+ 		
+ 		 if(donne.get("author") != null ){
+ 		 		String [] t = donne.get("author").split("and");
+			 	 A.SetAuteur(t[0]);
+			 	 
+		} 	 
+		if(donne.get("title") != null ){
+
+			 	 A.SetTitre(donne.get("title"));
+			 	 
+		} 		 	 
+		if(donne.get("booktitle") != null ){
+
+			 	 A.SetTitre(donne.get("booktitle"));
+			 	 
+		} 
+		if(donne.get("abstract") != null ){
+
+			 	 A.SetResume(donne.get("abstract"));
+			 	 
+		}
+		if(donne.get("note") != null ){
+
+			 	 A.SetDescriptionAuteur(donne.get("note"));
+			 	 
+		} 
+		if(donne.get("year") != null ){
+
+			 	 A.SetAnnee( Integer.parseInt(donne.get("year")));
+			 	 
+		}	 
+			 		 	 
+ 		return A;
+ 	
+ 	}
+ 	/**
+         * Renvoie le fichier dans un string. 
+	 *
+
+         */
  	public String FromFilePathToString(String Fichier){
  		String str="";
  		try{	
@@ -70,7 +166,7 @@ public class Bibtex {
  			
 		} catch (FileNotFoundException fnfe) {	
 		   		
-		 
+		   
 		} catch (IOException ex) {
 		
 		}
@@ -79,14 +175,23 @@ public class Bibtex {
  		return str;
  	
  	}
- 	public void lire(String Fichier){
+ 	/**
+         * lit un fichier bibtex et implemente la classe 
+	 *
+
+         */
+          public boolean ReadFromFile(String Fichier){
+          
+          	 	return	ReadFromString(FromFilePathToString(Fichier));
+          	
+          }
+          /**
+         * lit un String bibtex et implemente la classe 
+	 *
+
+         */
+ 	public boolean ReadFromString(String str){
  	
- 		String str=FromFilePathToString(Fichier);
-	
-	
-		
-		
-  		
   		String tampon="";
   		int mode=-1;
   		
@@ -110,7 +215,8 @@ public class Bibtex {
 		 			}
 		 			else{
 		 				if(tampon.equalsIgnoreCase("String")){//C'est une varaible de type string
-		 					 Extraire(str,str.charAt(i),String_var);
+		 					 if(!Extraire(str,str.charAt(i),String_var))
+		 					 	return false;
 		 				}
 		 				else{
 		 					type_de_doc=tampon;
@@ -124,7 +230,8 @@ public class Bibtex {
 					 			ref="";
 					 			i=in;
 					 		}
-		 					 Extraire(str,init,donne);
+		 					 if(!Extraire(str,init,donne))
+		 					 	return false;
 					 	}
 			 		}
 			 	}
@@ -136,10 +243,10 @@ public class Bibtex {
 		 	
 		 	
 		 }
-	
+		return true;
  	
  	}
- 	public String OnSenFou(String str,char init){
+ 	private String OnSenFou(String str,char init){ 	//c'est les comentaire et preambule 
  	
  		for(;i<str.length() && str.charAt(i) ==' ';i++);	//j'avance tant qu'il ya un espace
  		
@@ -181,7 +288,7 @@ public class Bibtex {
  	
  	
  	//------------>
- 	public boolean Extraire(String str,char init,Map<String, String> objet){
+ 	private boolean Extraire(String str,char init,Map<String, String> objet){
  	
  		String cle="";	
  		int nb_init=1;
@@ -194,7 +301,7 @@ public class Bibtex {
 	 		if(init == '{')
 	 			fin='}';
 	 		else 
-	 			return false;
+	 			return false;//bwef
  		
  		}
  		
@@ -204,10 +311,12 @@ public class Bibtex {
 					
 				i++;
 
-				if(i < str.length())		//Si c'est le fichier est pas finie
-					Valeur( str,fin,objet,cle);
+				if(i < str.length()){		//Si c'est le fichier est pas finie
+					if(!Valeur( str,fin,objet,cle))
+						return false;
+				}
 				else 
-					return false;
+					return false;//terminer sans fin
 
 				cle="";
 			}
@@ -236,7 +345,7 @@ public class Bibtex {
  	
  	
  	}
- 	public boolean Valeur(String str,char FIN_final,Map<String, String> objet,String cle){
+ 	private boolean Valeur(String str,char FIN_final,Map<String, String> objet,String cle){
  	
  		for(;i<str.length() && str.charAt(i) ==' ';i++);	//j'avance tant qu'il ya un espace
 		
@@ -310,7 +419,7 @@ public class Bibtex {
 						 
 				}	 
 
-				objet.put(cle,Tampon);
+				objet.put(cle.toLowerCase(),Tampon);
 
 				return true;
  				
@@ -345,29 +454,12 @@ public class Bibtex {
  	
  	}
  	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
- 	
+	
  	
 }
+
+
+
+
+
 
