e69d558d81398d8ad35cbdccab9b87c4346e130d
uapv1201185 <uapv1201185@ge-st3-120.univ-avignon.fr>
2017-04-27 09:13:22 +0200

    changement pour test final

--- src/textmining/Data_check.java
+++ src/textmining/Data_check.java
@@ -1,9 +1,10 @@
 package textmining;
 import java.util.Calendar;
+import java.lang.String;
 
 public class Data_check {
     /*
-     * Cette classe permet de verifié les donné extraite lors du text-mining (ou celle saisie par l'utilisateur)
+     * Cette classe permet de verifiées donné extraite lors du text-mining (ou celle saisie par l'utilisateur)
      * 
      * tolerence: nombre de caracteres speciaux maximum dans: hauteur, titre.
      * tolerencePourcentage: pourcentage de caracteres speciaux maximum dans le resumé/description auteur.
@@ -21,24 +22,27 @@ public class Data_check {
      *         
      *         
      *La verif Article verifie tt l'article si il est cohérent la fct renvoie 0 sinon elle renvoie -1
+     *
+     *@Author cedricales
      */
 	
 	
-    private static string caractereCoherent = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String caractereCoherent = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static int tolerence= 2;
     private static int tolerencePourcentage= 20;
     
     public static int checkarticle(Article a)
     {
     	int tmp=0;
-    	tmp = checkTitre(a.titleArt) + checkurl(a.urlPage) + checkurl(a.urlPdf) + checkAnnee(a.yearPub);
-    	for(int i=0; i<a.listAuthor.size() ;i++)
+    	//tmp = checkTitre(a.titleArt) + checkurl(a.urlPage) + checkurl(a.urlPdf) + checkAnnee(a.yearPub);
+    	tmp = checkTitre(a.mTitre) + checkAnnee(a.mAnnee);
+    	for(int i=0; i<a.mAuteur.size() ;i++)
     	{
-    		tmp+=checkAuteur(a.listAuthor.get(i));
+    		tmp+=checkAuteur(a.mAuteur.get(i));
     	}
-    	for(int i=0; i<a.listreference.size() ;i++)
+    	for(int i=0; i<a.mReferences.size() ;i++)
     	{
-    		tmp+=checkReference(a.listReference.get(i));
+    		tmp+=checkReference(a.mReferences.get(i));
     	}
     	if(tmp < 0) return -1;
     	else return 0;
@@ -54,7 +58,7 @@ public class Data_check {
 
     
     
-    public  static int checkTitre(string titre)
+    public  static int checkTitre(String titre)
     {    
         if( titre.length > 200) return -1;
         
@@ -72,7 +76,7 @@ public class Data_check {
     
     
     
-    public static int checkurl(string url)
+    public static int checkurl(String url)
     { 	
     	if( titre.length > 400) return -1;
     	int y=0;
@@ -94,7 +98,7 @@ public class Data_check {
     
     
     
-    public static int checkAuteur(string auteur)
+    public static int checkAuteur(String auteur)
     {
         if( auteur.length > 200) return -1;
         
@@ -112,7 +116,7 @@ public class Data_check {
     
     
     
-    public static int checkResume(string resume)
+    public static int checkResume(String resume)
     {
         int indice;
         int nbChar = 0;
@@ -130,7 +134,7 @@ public class Data_check {
     
     
     
-    public static int checkDescriptionAuteur(string descriptionAuteur)
+    public static int checkDescriptionAuteur(String descriptionAuteur)
     {
         int indice;
         int nbChar = 0;
@@ -150,12 +154,17 @@ public class Data_check {
     public static int checkAnnee(date annee)
     {
         Calendar c = Calendar.getInstance();
+        if(annee.getYear() > c.get(Calendar.YEAR)) return -5;
+        return 0;
+    }
+    public static int checkAnnee(int annee)
+    {
+        Calendar c = Calendar.getInstance();
         if(annee > c.get(Calendar.YEAR)) return -5;
         return 0;
     }
     
-    
-    public static int checkReference(string references)
+    public static int checkReference(String references)
     {
         int indice;
         int nbChar = 0;
