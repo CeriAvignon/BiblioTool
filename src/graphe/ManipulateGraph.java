2280ca1b885dc2ccc941b30e2ee05501fcc77b75
aitaazizeSoumaya <soumazize@gmail.com>
2017-05-04 19:38:49 +0200

    Add code graph_co-citant

--- src/graphe/ManipulateGraph.java
+++ src/graphe/ManipulateGraph.java
@@ -98,6 +98,60 @@ public class ManipulateGraph {
 	    DirectedGraph dg2 = MyGraph.updateGraph();// on mis a jour le graphe
 	     System.out.println("apres la mise a jour du graph");
 	     System.out.println("Nodes: " + dg1.getNodeCount() + " Edges: " + dg1.getEdgeCount());
+		
+		List<Node> selectedNodes=new ArrayList<Node>();
+		//retourner la liste des info du ou des noeuds sélectionnés
+		Node node1 = dg.getNode("1");
+		Node node2 = dg.getNode("2");
+		Node node3 = dg.getNode("3");
+
+		selectedNodes.add(node1);
+		selectedNodes.add(node2);
+		selectedNodes.add(node3);
+
+
+		List<String> listInfoSelectedNodes1=new ArrayList<String>();
+		listInfoSelectedNodes1=MyGraph.nodeInfo(selectedNodes);
+		System.out.println("");
+
+		int i=0;
+		for (String s : listInfoSelectedNodes1) {
+		//	System.out.println("Les information du noeud "+i+" sont :");
+			i++;
+		//	System.out.println("***"+s+"***");
+		}
+		//Cree un graph de cocitation
+		 ArrayList<ArrayList<Node>> cocitant=new  ArrayList<ArrayList<Node>>();
+		 ArrayList<Node> n=new ArrayList<Node> ();
+		 cocitant=MyGraph.returnNodesCocitant();
+		System.out.println("");
+		System.out.println("***Le resultat de regroupement des noeuds par cocitation (co_citant) ****");
+
+		for(int h =0;h<cocitant.size();h++)
+		{
+			n = cocitant.get(h);
+			System.out.println("***"+h+") ****");
+			for(int j=0;j<n.size();j++)
+
+			{
+				System.out.println("***"+n.get(j).getLabel()+"*****");
+
+				
+			}
+			
+		}
+		System.out.println("");
+
+		System.out.println("***Affichage des arcs qui relient les noeuds co_citant ****");
+
+		DirectedGraph dg2 = MyGraph.createUndirectedGraph2(cocitant);
+
+		for (Edge e : dg2.getEdges())
+		{
+	           System.out.println(e.getSource().getId() + " -> " + e.getTarget().getId());
+	    }
+
+
 	}
 	
 
