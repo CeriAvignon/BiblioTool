63d9618c21be6d477422d944831f0a75df3322cc
Faycal bouhara <faycal.bouhara@alumni.univ-avignon.fr>
2017-05-20 13:51:06 +0200

    fusionner les tache du groupe et tester un type de pdf le test est dans la classe test

--- src/textmining/TextMining.java
+++ src/textmining/TextMining.java
@@ -2,10 +2,13 @@ package textmining;
 
 
 
+
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.System;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 import com.itextpdf.text.pdf.PdfReader;
 
@@ -26,7 +29,8 @@ public class TextMining {
 	public static Article processPdf(String pdf)
 	{
 		Article a = new Article();
-		Meta_Donnes meta=new Meta_Donnes(pdf);
+		//Article a = new Article();
+		MetaDonnees meta=new MetaDonnees(pdf);
 		
 		//Récupérer les meta d'une classe à part (Décider de l'API (itextpdf,clownPDF ou ..?))
 		
@@ -38,7 +42,12 @@ public class TextMining {
 		if(meta.getDate().length() == 0) a.SetAnnee(-1);
 		else a.SetAnnee(Integer.parseInt(meta.getDate().substring(meta.getDate().length() - 4)));
 		a.SetSource("");
-		a.SetReference(reference);
+		ArrayList<ArrayList<String>> References = new ArrayList<ArrayList<String>>();
+		
+		AnalyseReferences analyseur = new AnalyseReferences(pdf);
+		ArrayList<Reference> r = analyseur.getReferences();
+		
+		a.AddReferences(r);
 		
 		return a;
 	}
--- src/textmining/test.java
+++ src/textmining/test.java
@@ -1,49 +1,26 @@
 package textmining;
 
 public class test {
- 
-	public static void main(String[] args) {
+	
+	
+	
+	
+		 public static void main(String[] args) {
 		
-		
-		// TODO Auto-generated method stub
-
-
-//tester avec un exemple 
-
-Article a=new Article(1, "aasqdqsd", "", "http://exo7.emath.fr/ficpdf/ficall.pdf", "", 2, 1, "2016", true) ;
-
-
-
-
-
-a.getUrlPage();
-
-
-Download d =new Download();
-//telecharger le pdf avec l'url existant dans la classe article
-d.getFile(a.getUrlPdf());
-
-//recuperer les meta donnée du pdf téléchargé
-
-Meta_Donnes meta=new Meta_Donnes(d.fileName);
-
-System.out.println(d.fileName);
-String auteur;
- auteur= meta.getAuteur();
- //mettre ajour la classe article avec les meta données 
- 
- //a.setListAuthor(auteur);
- a.setYearPub(meta.getDate());
- a.setTitle(meta.getTitre());
- 
- 
-System.out.println(auteur);
-System.out.println(meta.getTitre());
-System.out.println(a.getTittle());
-System.out.println(meta.getNbPage());
-System.out.println(meta.getSujet());
-System.out.println(meta.getCreateur());
-
-	}
+			 
+			Download d =new Download();
+			//telecharger le pdf reference avec les acolade 
+			d.getFile("http://hpac.gforge.inria.fr/Offres/PostdocEngineer-GrenobleLyonParis-HPAC-fr.pdf");	
+			
+			System.out.println("le nom de pdf est :"+d.fileName);
+			
+			//extraire les reference et metadonnées de pdf telecharger 
+			Article a = TextMining.processPdf(d.fileName);
+			 
+			 
+			 a.ViewArticle();
+			 
+			
+		 }
 
 }
